371edc8d4299f8536036318907d76a5df4033cce
Sergino Delia <serge716@gmail.com>
2015-01-19 07:56:56 -0500

    finished OO Tic Tac Toe

---
+++ oo_tic_tac_toe.rb
@@ -0,0 +1,147 @@
+# Tic Tac Toe description:
+# A board game with 2 players.  Start with an empty board with 9 (3 x 3) square
+# One player is "X" and the other player is "O"
+# Two Players alternate turns to mark empty squares, until a player gets 3 squares in a row
+# If all squares are marked and nobody wins its a tie
+
+# Nouns and Behaviors
+
+# board
+#   - squares
+#   - all squares marked
+#   - find all empty square 
+
+# player
+#   - name
+#   - marker
+
+# square
+#   - occupied?
+#   - mark(marker)
+
+# game engine
+#   - draw board
+#   - assign player to "X"
+#   - assign player to "O"
+
+class Board
+  WINNING_LINES = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
+
+  def initialize
+    @data = {}
+    (1..9).each {|position| @data[position] = Square.new(' ') }
+  end
+
+  def empty_squares
+    @data.select {|_, squares| square.empty?}.values
+  end
+
+  def empty_positions
+    @data.select {|_, square| square.empty?}.keys
+  end
+
+  def all_squares_marked?
+    @empty_squares == 0
+  end
+
+  def three_squares_in_a_row?(marker)
+    WINNING_LINES.each do |line|
+      return true if @data[line[0]].value == marker && @data[line[1]].value == marker && @data[line[2]].value == marker
+    end
+    false
+  end 
+
+  def mark_square(position, marker)
+    @data[position].mark(marker)
+  end
+
+  def draw
+    system 'clear'
+    puts " #{@data[1]} | #{@data[2]} | #{@data[3]} "
+    puts "---+---+---"
+    puts " #{@data[4]} | #{@data[5]} | #{@data[6]} "
+    puts "---+---+---"
+    puts " #{@data[7]} | #{@data[8]} | #{@data[9]} "
+  end
+end
+
+class Player
+  attr_reader :marker, :name
+  def initialize(name, marker)
+    @name = name
+    @marker = marker
+  end
+end
+
+class Square
+  attr_reader :value
+
+  def initialize(value)
+    @value = value
+  end
+
+  def empty?
+    @value == " "
+  end
+
+  def mark(marker)
+    @value = marker
+  end
+
+  def to_s
+    @value
+  end
+end
+
+class Game
+  def initialize
+    @board = Board.new
+    @human = Player.new("John", "X")
+    @computer = Player.new("CPU", "O")
+    @current_player = @human
+  end
+
+  def current_player_marks_square
+    if @current_player == @human
+      begin
+        puts "Choose a position #{@board.empty_positions} to place a piece:"
+        position = gets.chomp.to_i
+      end until @board.empty_positions.include?(position)
+    else
+      position = @board.empty_positions.sample
+    end
+    @board.mark_square(position, @current_player.marker)
+  end
+
+  def alternate_player
+    if @current_player == @human
+      @current_player = @computer
+    else
+      @current_player = @human
+    end
+  end
+
+  def current_player_wins?
+    @board.three_squares_in_a_row?(@current_player.marker)
+  end
+
+  def play
+    @board.draw
+    loop do 
+      current_player_marks_square
+      @board.draw
+      if current_player_wins?
+        puts "The winner is #{@current_player.name}"
+        break
+      elsif @board.all_squares_marked?
+        puts "It's a tie"
+        break
+      else
+        alternate_player
+      end
+    end
+    puts "Bye"
+  end
+end
+
+Game.new.play
